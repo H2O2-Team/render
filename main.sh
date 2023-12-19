9def3d453f04a8af7d83291ec00a2ed4afdc1f8b
502 Bad Gateway <128957490+502BADGATEWAY-Official@users.noreply.github.com>
2023-12-20 02:41:12 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt -y upgrade  && apt install -y curl wget git screen nano vim neofetch net-tools ssh sudo systemctl nodejs npm 
+apt update && apt -y upgrade  && apt install -y curl wget git screen nano vim neofetch net-tools ssh sudo systemctl nodejs npm openjdk-17-jdk openjdk-17-jre
